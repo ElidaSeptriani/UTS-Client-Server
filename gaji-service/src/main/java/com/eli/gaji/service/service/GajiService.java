b55a5b4be548c7e91b5ceba8cbd6e5e0505dba5e
ElidaSeptriani <113493736+ElidaSeptriani@users.noreply.github.com>
2023-01-03 19:27:39 +0700

    Add files via upload

--- gaji-service/src/main/java/com/eli/gaji/service/service/GajiService.java
+++ gaji-service/src/main/java/com/eli/gaji/service/service/GajiService.java
@@ -4,31 +4,10 @@
  */
 package com.eli.gaji.service.service;
 
-import com.eli.gaji.service.VO.Pegawai;
-import com.eli.gaji.service.VO.ResponseTemplateVO;
-import com.eli.gaji.service.entity.Gaji;
-import com.eli.gaji.service.repository.GajiRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.client.RestTemplate;
-
 /**
  *
  * @author ASUS
  */
 public class GajiService {
-    @Autowired
-    private GajiRepository gajiRepository;
     
-    @Autowired
-    private RestTemplate restTemplate;
-    
-    public Gaji saveGaji(Gaji gaji) {
-        return gajiRepository.save(gaji);
-    }
-    
-    public ResponseTemplateVO getGaji(Long gajiId){
-        ResponseTemplateVo vo = new ResponseTemplateVO();
-        Gaji gaji = gajiRepository.findByGajiId(gajiId);
-        Pegawai pegawai = restTemplate.getForObject(, responseType)
-    }
 }
